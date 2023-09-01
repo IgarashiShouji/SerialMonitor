10b340e8f47660e23d65a44a60990b69d59b0905
Shouji.Igarashi <Shouji.Igarashi@gmail.com>
2023-09-01 02:37:53 +0000

    add test4.rb

---
+++ test-script/test4.rb
@@ -0,0 +1,15 @@
+print "test\n"
+
+bin = BinEdit.new('0102030405')
+printf("%d: %s\n", bin.length, bin.dump)
+printf("%d: %s\n", bin.length, bin.dump(1, 5))
+printf("%d: %s\n", bin.length, bin.dump(3))
+
+print BinEdit.hexToArray('bb sS wW iI dD fF h2 A3', '0102 FEFF FFFE 0100 0100 FCFFFFFF FFFFFFFC 01000000 00000001 12345678 12345678 5566 303132'), "\n"
+
+bin = BinEdit.new(3)
+printf("wsize: %s\n", bin.write('11 22'))
+printf("wsize: %s\n", bin.write(2, '03'))
+printf("%d: %s\n", bin.length, bin.dump)
+
+print "test end\n"
