71f542a946369d97d1f22252ea99672fe28edf50
Shouji.Igarashi <Shouji.Igarashi@gmail.com>
2024-12-02 17:46:10 +0900

    debug

--- build-OpenXLSX.sh
+++ build-OpenXLSX.sh
@@ -5,8 +5,10 @@ if [ ! -d OpenXLSX ]; then
   git clone https://github.com/troldal/OpenXLSX.git
   cd OpenXLSX
   #git checkout 1f58d038aac9319026c0fd5b4efe4af2cd4b45ba -b test
+  #git checkout 680267d4c5d105bf5f8e3124c1b8220b0dba06a9 -b test
+  git checkout f85f7f1bd632094b5d78d4d1f575955fc3801886 -b test
 
-  git checkout b80da42d1454f361c29117095ebe1989437db390 -b test
+  #git checkout b80da42d1454f361c29117095ebe1989437db390 -b test
   cd ..
 fi
 [ ! -d OpenXLSX/build ] && mkdir -p OpenXLSX/build
