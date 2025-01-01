f02e19cf1b9e18b293e20786123d28544fb1b1a4
Shouji.Igarashi <Shouji.Igarashi@gmail.com>
2025-01-01 22:02:30 +0900

    change boost to posix for SerialControl on linux

--- Source/linux-api.cpp
+++ Source/linux-api.cpp
@@ -1,5 +1,5 @@
 /**
- * Serial Control Class on boost library
+ * Serial Control Class on linux system
  *
  * @file    SerialControl.cpp
  * @brief   Serial Control Class
@@ -14,7 +14,6 @@
 
 #include <algorithm>
 #include <array>
-#include <boost/asio.hpp>
 #include <chrono>
 #include <cstdio>
 #include <filesystem>
@@ -27,64 +26,27 @@
 #include <string>
 #include <thread>
 #include <unistd.h>
+#include <linux/serial.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <termios.h>
 
 
 using namespace MyEntity;
-using namespace boost::asio;
 using namespace std;
 
 
-/**
- * convert to UTF8 string from CP932
- *
- * @param   dst
- * @param   icd
- * @param   src
- * @param   size
- */
-static void toUTF8(string & dst, iconv_t & icd, char * src, size_t size)
-{
-    size_t sz = size;
-    while(0 < sz)
-    {
-        char utf8[size*2];
-        char * p_dst = utf8;
-        size_t sz_utf8 = sizeof(utf8);
-        iconv(icd, &src, &sz, &p_dst, &sz_utf8);
-        *p_dst = '\0';
-        dst += utf8;
-    }
-}
-
-
 /* --------------------------------------------------------------------------------<< Serial Control >>-------------------------------------------------------------------------------- */
-class RtsContorl
+class SerialControlLinux : public SerialControl
 {
 protected:
-    int     fd;
-    bool    ctrl;
-    bool    rts;
-public:
-    RtsContorl(int _fd, bool _ctrl) : fd(_fd), ctrl(_ctrl), rts(false) { }
-    virtual ~RtsContorl(void)                                          { }
-    virtual void set(void)              { if(ctrl) { int data = TIOCM_RTS; ioctl(fd, TIOCMBIS, &data); } rts = true;   }
-    virtual void clear(void)            { if(ctrl) { int data = TIOCM_RTS; ioctl(fd, TIOCMBIC, &data); } rts = false;  }
-    virtual bool status(void) const     { if(ctrl) { return rts; } return false; }
-};
-
-class SerialControlBoost : public SerialControl
-{
-protected:
-    boost::asio::io_service     io;
-    boost::asio::serial_port    port;
-    unsigned int                _baudrate;
-    unsigned char               bit_num;
-    RtsContorl                  rts;
-    int                         fd_pipe[2];
+    int fd;
+    bool ctrl;
+    bool rts;
 
 public:
-    SerialControlBoost(const char * name, unsigned int bd, Parity pt, StopBit st, bool rts_ctrl);
-    virtual ~SerialControlBoost(void);
+    SerialControlLinux(const char * name, unsigned int bd, Parity pt, StopBit st, bool rts_ctrl);
+    virtual ~SerialControlLinux(void);
     virtual std::size_t read(unsigned char * data, std::size_t size);
     virtual std::size_t send(unsigned char * data, std::size_t size);
     virtual bool rts_status(void) const;
@@ -92,92 +54,131 @@ public:
     virtual void clearRTS(void);
     virtual void close(void);
 };
-
-SerialControlBoost::SerialControlBoost(const char * name, unsigned int bd, Parity pt, StopBit st, bool rts_ctrl)
-  : port(io, name), _baudrate(bd), bit_num(1 + 8 + st), rts(port.native_handle(), rts_ctrl)
+SerialControlLinux::SerialControlLinux(const char * name, unsigned int bd, Parity pt, StopBit st, bool rts_ctrl)
+  : fd(-1), ctrl(rts_ctrl), rts(false)
 {
-    auto result =  pipe(fd_pipe);
-    if(pt != none) bit_num ++;
-    port.set_option(serial_port_base::baud_rate(_baudrate));
-    port.set_option(serial_port_base::character_size(8));
-    port.set_option(serial_port_base::flow_control(serial_port_base::flow_control::none));
-    switch(pt)
+#if 0
+50	B50     1200	B1200       57600	B57600      1000000	B1000000
+75	B75     1800	B1800       115200	B115200     1152000	B1152000
+110	B110    2400	B2400       230400	B230400     1500000	B1500000
+134	B134    4800	B4800       460800	B460800     2000000	B2000000
+150	B150    9600	B9600       500000	B500000     2500000	B2500000
+200	B200    19200	B19200      576000	B576000     3000000	B3000000
+300	B300    38400	B38400      921600	B921600     3500000	B3500000
+600	B600                                            4000000	B4000000
+#endif
+    fd = open(name, O_RDWR | O_NOCTTY | O_NONBLOCK);
+    if(0 <= fd)
     {
-        case none:  port.set_option(serial_port_base::parity(serial_port_base::parity::none));  break;
-        case odd:   port.set_option(serial_port_base::parity(serial_port_base::parity::odd));   break;
-        case even:  port.set_option(serial_port_base::parity(serial_port_base::parity::even));  break;
-        default: break;
-    }
-    switch(st)
-    {
-        case one:   port.set_option(serial_port_base::stop_bits(serial_port_base::stop_bits::one)); break;
-        case two: port.set_option(serial_port_base::stop_bits(serial_port_base::stop_bits::two));   break;
-        default: break;
-    }
-    clearRTS();
-}
+        // シリアルポートの設定
+        struct termios tty;
+        if(0 == tcgetattr(fd, &tty))
+        {
+            cfsetospeed(&tty, bd); // 出力ボーレート
+            cfsetispeed(&tty, bd); // 入力ボーレート
 
-static void fdclose(int fd)
-{
-    close(fd);
+            tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8; // 8ビットデータ
+            tty.c_iflag &= ~IGNBRK;                     // ブレークを無視しない
+            tty.c_lflag = 0;                            // 非カノニカルモード
+            tty.c_oflag = 0;                            // ローデータ出力
+            tty.c_cc[VMIN] = 1;                         // 最低1文字の入力を要求
+            tty.c_cc[VTIME] = 0;                        // タイムアウト無効
+            switch(pt)
+            {
+            case odd:
+                tty.c_cflag |= PARENB;
+                tty.c_cflag |= PARODD;
+                break;
+            case even:
+                tty.c_cflag |= PARENB;
+                tty.c_cflag &= ~PARODD;
+                break;
+            case none:
+            default:
+                tty.c_cflag &= ~PARENB;
+                break;
+            }
+            switch(st)
+            {
+            case one:   tty.c_cflag &= ~CSTOPB; break;
+            case two:   tty.c_cflag |= CSTOPB;  break;
+            }
+            if(0 != tcsetattr(fd, TCSANOW, &tty)) { close(); }
+        }
+        else { close(); }
+    }
 }
-SerialControlBoost::~SerialControlBoost(void)
+SerialControlLinux::~SerialControlLinux(void)
 {
     close();
-    fdclose(fd_pipe[0]);
-    fdclose(fd_pipe[1]);
 }
-
-void SerialControlBoost::close(void)
+std::size_t SerialControlLinux::send(unsigned char * data, std::size_t size)
 {
-    static const uint8_t cmd = 1;
-    auto w_size = write(fd_pipe[1], &cmd, sizeof(cmd));
-    port.close();
-}
-
-std::size_t SerialControlBoost::read(unsigned char * data, std::size_t size)
-{
-    std::size_t len = 0;
-    try
+    size_t wr_size = 0;
+    if(0 <= fd)
     {
-        if( port.is_open() )
-        {
-            auto fd = port.native_handle();
-            fd_set fds;
-            FD_ZERO(&fds);
-            FD_SET(fd, &fds);
-            FD_SET(fd_pipe[0], &fds);
-            auto max =fd; 
-            if( max < fd_pipe[0] ) { max = fd_pipe[0]; }
-            auto result = select( max+1, &fds, NULL, NULL, NULL );
-            if( FD_ISSET(fd_pipe[0], &fds) )    { return 0; }
-            if( FD_ISSET(fd, &fds) )            { len = port.read_some(buffer(data, size)); }
-        }
-    } catch(...) { }
-    return len;
-}
-
-std::size_t SerialControlBoost::send(unsigned char * data, std::size_t size)
-{
-    if(0 < size)
-    {
-        setRTS();
-        unsigned int    send_time = (1000 * bit_num * (size+1)) / _baudrate;
-        std::size_t     wlen      = port.write_some(buffer(data, size));
-        std::this_thread::sleep_for(std::chrono::milliseconds(send_time));
-        clearRTS();
-        return wlen;
+        wr_size = ::write(fd, data, size);
     }
-    return 0;
+    return wr_size;
 }
+std::size_t SerialControlLinux::read(unsigned char * data, std::size_t size)
+{
+    size_t rd_size = 0;
+    if(0 <= fd)
+    {
 
-bool SerialControlBoost::rts_status(void) const { return rts.status(); }
-void SerialControlBoost::setRTS(void)           { rts.set();   }
-void SerialControlBoost::clearRTS(void)         { rts.clear(); }
+        fd_set readfds;
+        FD_ZERO(&readfds);
+        FD_SET(fd, &readfds);    // シリアル通信を追加
+        int max_fd = fd;
+
+        // `select` を呼び出し
+        int activity = select(max_fd + 1, &readfds, nullptr, nullptr, nullptr);
+        if(0 <= activity)
+        {
+            if(FD_ISSET(fd, &readfds))
+            {
+                rd_size = ::read(fd, data, size);
+            }
+        }
+    }
+    return rd_size;
+}
+bool SerialControlLinux::rts_status(void) const
+{
+    if(ctrl) { return rts; }
+    return false;
+}
+void SerialControlLinux::setRTS(void)
+{
+    if(ctrl)
+    {
+        int data = TIOCM_RTS;
+        ioctl(fd, TIOCMBIS, &data);
+    }
+    rts = true;
+}
+void SerialControlLinux::clearRTS(void)
+{
+    if(ctrl)
+    {
+        int data = TIOCM_RTS;
+        ioctl(fd, TIOCMBIC, &data);
+    }
+    rts = false;
+}
+void SerialControlLinux::close(void)
+{
+    if(0 <= fd)
+    {
+        ::close(fd);
+        fd = -1;
+    }
+}
 
 SerialControl * SerialControl::createObject(const string & name, unsigned int baud, Parity pt, StopBit st, bool rts)
 {
-    SerialControl * com = new SerialControlBoost(name.c_str(), baud, pt, st, rts);
+    SerialControl * com = new SerialControlLinux(name.c_str(), baud, pt, st, rts);
     return com;
 }
 
@@ -266,3 +267,80 @@ std::list<std::string> & PipeList::ref(void)
 {
     return list;
 }
+
+
+
+
+#if 0
+void test(void)
+{
+        // シリアルポートを開く
+    const char* serial_port = "/dev/ttyS0"; // 使用するシリアルポートを指定
+    int serial_fd = open(serial_port, O_RDWR | O_NOCTTY | O_NONBLOCK);
+    if (serial_fd < 0) {
+        std::cerr << "Failed to open serial port: " << strerror(errno) << std::endl;
+        return;
+    }
+
+    // シリアルポートの設定
+    struct termios tty;
+    if (tcgetattr(serial_fd, &tty) != 0) {
+        std::cerr << "Failed to get serial attributes: " << strerror(errno) << std::endl;
+        close(serial_fd);
+        return;
+    }
+
+    cfsetospeed(&tty, B9600); // 出力ボーレート
+    cfsetispeed(&tty, B9600); // 入力ボーレート
+
+    tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8; // 8ビットデータ
+    tty.c_iflag &= ~IGNBRK;                     // ブレークを無視しない
+    tty.c_lflag = 0;                            // 非カノニカルモード
+    tty.c_oflag = 0;                            // ローデータ出力
+    tty.c_cc[VMIN] = 1;                         // 最低1文字の入力を要求
+    tty.c_cc[VTIME] = 0;                        // タイムアウト無効
+
+    if (tcsetattr(serial_fd, TCSANOW, &tty) != 0) {
+        std::cerr << "Failed to set serial attributes: " << strerror(errno) << std::endl;
+        close(serial_fd);
+        return;
+    }
+
+    // `select` で複数の入力を待つ
+    while (true) {
+        fd_set readfds;
+        FD_ZERO(&readfds);
+        FD_SET(fileno(stdin), &readfds); // 標準入力を追加
+        FD_SET(serial_fd, &readfds);    // シリアル通信を追加
+
+        int max_fd = std::max(fileno(stdin), serial_fd);
+
+        // `select` を呼び出し
+        int activity = select(max_fd + 1, &readfds, nullptr, nullptr, nullptr);
+        if (activity < 0) {
+            std::cerr << "select() error: " << strerror(errno) << std::endl;
+            break;
+        }
+
+        // 標準入力からのデータを処理
+        if (FD_ISSET(fileno(stdin), &readfds)) {
+            char buffer[256];
+            if (fgets(buffer, sizeof(buffer), stdin) != nullptr) {
+                std::cout << "Standard Input: " << buffer;
+            }
+        }
+
+        // シリアル通信からのデータを処理
+        if (FD_ISSET(serial_fd, &readfds)) {
+            char buffer[256];
+            ssize_t bytes_read = read(serial_fd, buffer, sizeof(buffer) - 1);
+            if (bytes_read > 0) {
+                buffer[bytes_read] = '\0';
+                std::cout << "Serial Input: " << buffer;
+            }
+        }
+    }
+
+    close(serial_fd);
+}
+#endif
