22209b5334e46fa63c4245313071e06c272ef240
Shouji.Igarashi <Shouji.Igarashi@gmail.com>
2025-02-27 17:08:46 +0900

    Rev.0.15.00: create new Revision

--- Source/main.cpp
+++ Source/main.cpp
@@ -852,7 +852,7 @@ static void mrb_xlsx_context_free(mrb_state * mrb, void * ptr);
 
 
 /* -- static tables -- */
-static const char *  SoftwareRevision = "0.14.19";
+static const char *  SoftwareRevision = "0.15.00";
 static const struct mrb_data_type mrb_core_context_type =
 {
     "mrb_core_context",         mrb_core_context_free
